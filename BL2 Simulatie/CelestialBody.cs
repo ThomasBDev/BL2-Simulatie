9b3f0ba3dbc57ae573ee9d083054c83d8460b3cc
Thomas van Deukeren <t.b.deukeren@students.uu.nl>
2018-04-14 13:52:41 +0200

    no message

--- BL2 Simulatie/CelestialBody.cs
+++ BL2 Simulatie/CelestialBody.cs
@@ -8,7 +8,7 @@ using Microsoft.Xna.Framework.Graphics;
 
 namespace BL2_Simulatie
 {
-    public class Ster
+    public class CelestialBody
     {
         public float scale, rotationSpeed, scaledWidth, scaledHeight;
         public double mass, velocity, direction;
@@ -17,7 +17,7 @@ namespace BL2_Simulatie
         public Vector2 position, rotationPoint;
         public Texture2D sprite;
 
-        public Ster(float scale, float rotation, Texture2D sprite, double mass, double velocity, double direction)
+        public CelestialBody(float scale, float rotation, Texture2D sprite, double mass, double velocity, double direction)
         {
             this.scale = scale;
             rotationSpeed = rotation;
@@ -25,7 +25,7 @@ namespace BL2_Simulatie
             scaledWidth = sprite.Width * scale;
             scaledHeight = sprite.Height * scale;            
             rotationPoint = new Vector2(sprite.Width / 2, sprite.Height / 2);
-            this.mass = mass;
+            this.mass = mass * 10000000000;
             this.velocity = velocity;
             this.direction = direction;
         }
--- BL2 Simulatie/Game1.cs
+++ BL2 Simulatie/Game1.cs
@@ -11,14 +11,36 @@ namespace BL2_Simulatie
         GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
 
-        Vector2 oorsprong;
-        Ster zon, aarde, aarde2;
-        Kracht MPZ;
+        Vector2 origin, centerScreen;
+        CelestialBody star, pandora;
 
         Formules form = new Formules();
 
         float screenWidth, screenHeight;
+
         double tijdstap = 1;
+        double metersPerPixel = 467140935;
+        double a = 163499327000;
+        double b = 162957623016;
+        double c = 13298234111;
+        double aPix, bPix, cPix;
+
+        //double massaZon = 20154851311000000000000000000;
+        //double massaPandora = 6389658258000000000000000;
+
+        //10 keer kleiner vanwege te groot getal.
+        double massaZon = 2015485131100000000;
+        double massaPandora = 638965825800000;
+        double omloopSnelheid = 3257.536051;
+ 
+        bool test = false;
+        bool test2 = false;
+        bool test3 = false;
+        bool resultaatOutput = true;
+        int metingNummer = 1;
+        string lijn = "====================================================";
+
+
 
         public Game1()
         {
@@ -28,18 +50,29 @@ namespace BL2_Simulatie
 
         protected override void Initialize()
         {
-            oorsprong = Vector2.Zero;
+            origin = Vector2.Zero;
 
-            //Standaard = (800, 480)
+            //Laptop scherm afmetingen? = (1536, 864)
             int fullscreenWidth = GraphicsDevice.DisplayMode.Width;
             int fullscreenHeight = GraphicsDevice.DisplayMode.Height;
 
-            graphics.PreferredBackBufferWidth = 1400;
-            graphics.PreferredBackBufferHeight = 720;
+            if (test)
+            {
+                Print();
+                Print(lijn);
+                Print("Max = " + double.MaxValue);
+                Print(graphics.PreferredBackBufferWidth + ", " + graphics.PreferredBackBufferHeight);
+            }
+
+            //Standaard = (800, 480)
+            //Grootste window = (1400, 720)
+            graphics.PreferredBackBufferWidth = 700;
+            graphics.PreferredBackBufferHeight = 700;
             graphics.ApplyChanges();
 
             screenWidth = graphics.PreferredBackBufferWidth;
             screenHeight = graphics.PreferredBackBufferHeight;
+            centerScreen = new Vector2(screenWidth / 2, screenHeight / 2);
 
             int windowPositionX = (int)((fullscreenWidth / 2) - (screenWidth / 2));
             int windowPositionY = (int)((fullscreenHeight / 2) - (screenHeight / 2));
@@ -47,9 +80,21 @@ namespace BL2_Simulatie
             this.Window.Position = new Point(windowPositionX, windowPositionY);
             this.IsMouseVisible = true;
 
-            //Print("fullscreenWidth, fullscreenHeight = " + fullscreenWidth + ", " + fullscreenHeight);
-            //Print("screenWidth, screenHeight = " + screenWidth + ", " + screenHeight);
-            //Print("windowPositionX, windowPositionY = " + windowPositionX + ", " + windowPositionY);
+            aPix = a / metersPerPixel;
+            bPix = b / metersPerPixel;
+            cPix = c / metersPerPixel;
+
+            if (test)
+            {
+                Print();
+                Print("middenScherm = " + centerScreen);
+                Print("fullscreenWidth, fullscreenHeight = " + fullscreenWidth + ", " + fullscreenHeight);
+                Print("screenWidth, screenHeight = " + screenWidth + ", " + screenHeight);
+                Print("windowPositionX, windowPositionY = " + windowPositionX + ", " + windowPositionY);
+                Print("aPix = " + aPix);
+                Print("bPix = " + bPix);
+                Print("cPix = " + cPix);
+            }
 
             base.Initialize();
         }
@@ -59,18 +104,19 @@ namespace BL2_Simulatie
             spriteBatch = new SpriteBatch(GraphicsDevice);
 
             //scale, rotation, sprite.
-            zon = new Ster(0.1f, 0f, Content.Load<Texture2D>("sprites/spr_sun"), 1000, 10, Math.PI);
+            star = new CelestialBody(0.05f, 0f, Content.Load<Texture2D>("sprites/spr_sun"), massaZon, 0, 0);
             //Door position later aan te maken, is het mogelijk om variabeles van Ster te gebruiken in de position berekening.
-            //zon.position = new Vector2(screenWidth / 2, screenHeight / 2);
-            zon.position = new Vector2(-10, 0);
+            star.position = new Vector2(centerScreen.X + (float)cPix, centerScreen.Y);
 
-            aarde = new Ster(0.5f, 0f, Content.Load<Texture2D>("sprites/spr_earth"), 10, 10, 0);
-            aarde.position = new Vector2(0, 0);
+            pandora = new CelestialBody(0.01f, 0f, Content.Load<Texture2D>("sprites/spr_sun"), massaPandora, omloopSnelheid, 0);
+            pandora.position = new Vector2(centerScreen.X + (float)aPix, centerScreen.Y);
 
-            //aarde2 = new Ster(0.5f, 0f, Content.Load<Texture2D>("sprites/spr_earth"), 10, 1000);
-            //aarde2.position = new Vector2(screenWidth * 0.25f, screenHeight / 2);
-
-            MPZ = new Kracht(Math.PI * 0.75, 1);
+            if (test2)
+            {
+                Print();
+                Print("zon.position = " + star.position);
+                Print("aarde.position = " + pandora.position);
+            }
         }
 
         protected override void UnloadContent()
@@ -102,16 +148,15 @@ namespace BL2_Simulatie
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
             spriteBatch.Begin();
-            DrawSprite(zon, Color.White);
-            DrawSprite(aarde, Color.White);
-            //DrawSprite(aarde2, Color.White);
+            DrawSprite(star, Color.White);
+            DrawSprite(pandora, Color.Green);
             spriteBatch.End();
 
             base.Draw(gameTime);
         }
 
         //Een methode om spriteBatch.Draw iets compacter te maken.
-        public void DrawSprite(Ster gameObject, Color color)
+        public void DrawSprite(CelestialBody gameObject, Color color)
         {
             spriteBatch.Draw(gameObject.sprite, gameObject.position, null, color, gameObject.rotationSpeed, gameObject.rotationPoint, gameObject.scale, SpriteEffects.None, 0f);
         }
@@ -132,31 +177,43 @@ namespace BL2_Simulatie
                 //aarde.direction += (float)form.NieuweRichting(aarde.direction, MPZ.power);
                 //Print("direction = " + aarde.direction + " ==> " + (aarde.direction / Math.PI) + " PI.");
 
-                double d = form.AfstandTussenHemellichamen(aarde.position, zon.position);
-                double Fvel = form.Fv(aarde.mass, aarde.velocity, tijdstap);
-                double Fmiddel = form.Fmpz(aarde.mass, aarde.velocity, d);
+                double d = form.AfstandTussenHemellichamen(pandora.position, star.position) * metersPerPixel;
+                double Fvel = form.Fv(pandora.mass, pandora.velocity, tijdstap);
+                double Fmiddel = form.Fmpz(pandora.mass, pandora.velocity, d);
 
-                Print("d = " + d);
-                Print("Fvel = " + Fvel);
-                Print("Fmpz = " + Fmiddel);
+                if (test3)
+                {
+                    Print(lijn);
+                    Print("d = " + d);
+                    Print("(Fvel, Fmpz) = (" + Fvel + ", " + Fmiddel + ")");
+                }
 
-                Print("Oude aarde.direction = " + aarde.direction);
-                aarde.direction -= (form.NieuweRichting(Fvel, Fmiddel));
-                Print("Nieuwe aarde.direction = " + aarde.direction);
-                //aarde.rotationSpeed = (float)aarde.direction / 100;
+                double oudDir = pandora.direction;
+                //Linksom draaien
+                pandora.direction -= (form.NieuweRichting(Fvel, Fmiddel));
 
                 double Fres = Math.Sqrt(Math.Pow(Fvel, 2) + Math.Pow(Fmiddel, 2));
-                double Fx = form.Fx(Fres, aarde.direction);
-                double Fy = form.Fy(Fres, aarde.direction);
-                Print("Fres = " + Fres);
-                Print("Fx = " + Fx);
-                Print("Fy = " + Fy);
+                double Fx = form.Fx(Fres, pandora.direction);
+                double Fy = form.Fy(Fres, pandora.direction);
 
-                Print("Oude position = " + aarde.position);
-                aarde.position.X += (float)form.FmaVerplaatsing(Fx, aarde.mass, tijdstap);
-                aarde.position.Y -= (float)form.FmaVerplaatsing(Fy, aarde.mass, tijdstap);
-                Print("Nieuwe position = " + aarde.position);
-                Print();
+                Vector2 oudPos = pandora.position;
+                pandora.position.X += (float)form.FmaVerplaatsing(Fx, pandora.mass, tijdstap);
+                pandora.position.Y -= (float)form.FmaVerplaatsing(Fy, pandora.mass, tijdstap);
+
+                if (test3)
+                {
+                    Print("(Oude aarde.direction, Nieuwe aarde.direction) = (" + oudDir + ", " + pandora.direction + ")");
+                    Print("Fres = " + Fres);
+                    Print("(Fx, Fy) = (" + Fx + ", " + Fy + ")");
+                    Print("Oude position, Nieuwe position = " + oudPos + ", " + pandora.position);
+                    Print();
+                }
+
+                if (resultaatOutput)
+                {
+                    Print("Meting " + metingNummer + " = " + pandora.position);
+                    metingNummer++;
+                }
             }
         }
     }
